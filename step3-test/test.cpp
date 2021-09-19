ace38586fbbdf141f89846075019026d0297cf73
vkhristenko <vdkhristenko1991@gmail.com>
2021-09-19 19:31:24 +0200

    cmake packaging seems to work

--- step3-test/test.cpp
+++ step3-test/test.cpp
@@ -1,10 +1,12 @@
 #include <iostream>
 
 #include "test1.hpp"
+#include "test2.hpp"
 
 int main(int argc, char** argv) {
     std::cout << argv[0] << std::endl;
     std::cout << "test1 = " << test1() << std::endl;
+    std::cout << "test2 = " << test2() << std::endl;
 
     return 0;
 }
--- step3/MathFunctions/test2.cpp
+++ step3/MathFunctions/test2.cpp
@@ -1,3 +1,5 @@
 #include "test2.hpp"
 
+#include "Table.h"
+
 int test2() { return 2; }
